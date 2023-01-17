37d880950188e19d64aa60263ff09b156f81e682
himanshu07pednekar10 <122275448+himanshu07pednekar10@users.noreply.github.com>
2023-01-17 10:31:20 +0530

    Add files via upload

---
+++ SortNumber.js
@@ -0,0 +1,4 @@
+var numericArray = [11, 3, 4, 15, 5, 0, 1];
+var sortedArray = numericArray.sort(function (n1, n2) { return n1 - n2; });
+console.log(sortedArray);
+//# sourceMappingURL=SortNumber.js.map
